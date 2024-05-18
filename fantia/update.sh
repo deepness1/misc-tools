4049326bfc874268aa1e61483b9321f03a2a746e
mojyack <mojyack@gmail.com>
2024-05-18 19:57:13 +0900

    fantia: add users-movie.csv

--- fantia/update.sh
+++ fantia/update.sh
@@ -2,3 +2,4 @@ saveroot=$HOME/working/dl/repository
 recordroot=$HOME/working/dl/repository-records
 
 python src/dump.py users.csv $saveroot/comic $recordroot/comic
+python src/dump.py users-movie.csv $saveroot/movie $recordroot/movie
