c70ecb370e28c28b35dcdcf7677fc505b3fef54e
mojyack <mojyack@gmail.com>
2024-11-30 22:44:46 +0900

    kemono: refactor using api

--- kemono/src/kemono.py
+++ kemono/src/kemono.py
@@ -2,6 +2,7 @@ import os
 import sys
 import threading
 import urllib
+import json
 
 from bs4 import BeautifulSoup
 import strlib
@@ -19,109 +20,92 @@ def build_full_url(path):
         return origin + path
 
 
-def make_abs_link(link):
-    if link[0] == "/":
-        link = "https://kemono.su" + link
-    return link
-
-
 class Post:
     def __init__(self, site, uid, pid):
         self.pid = pid
 
-        url = f"{origin}/{site}/user/{uid}/post/{pid}"
+        url = f"{origin}/api/v1/{site}/user/{uid}/post/{pid}"
         res = fetch.request(url)
-        bs = BeautifulSoup(res.text, "html.parser")
-        section = bs.body.main.section
-        post_info = section.header.find("div", class_="post__info")
-        self.title = post_info.find("h1", class_="post__title").find_all("span")[0].text
-        info_time = post_info.find("time")
-        if info_time != None:
-            self.date = info_time.text.strip()
-        else:
-            published = bs.head.find("meta", attrs={"name": "published"})
-            self.date = published.get("content")[:10]
+        root = json.loads(res.text)
+        # TODO: support multiple revisions
+        # props = root["props"]
+        # for num, r in props["revisions"]:
+        #     if num == int(props["flagged"]):
+        #         rev = r
+        rev = root["post"]
+        self.title = rev["title"]
+        self.date = rev["added"][:10]
 
-        post_body = section.find("div", class_="post__body")
-        content = post_body.find("div", class_="post__content")
+        if "url" in rev["embed"]:
+            self.embed = rev["embed"]
+        else:
+            self.embed = None
+
         self.files = []
-        if content != None:
-            if content.string != None:
-                self.description = content.string
-            else:
-                self.description = ""
-                pre = content.find("pre")
-                if pre != None:
-                    self.description += pre.text.strip()
-                ps = content.find_all(["p", "h3"])
-                if len(ps) == 0:
-                    ps = [content]
-                for p in ps:
-                    for e in p:
-                        text = str(e.string).strip()
-                        match e.name:
-                            case None | "strong" | "span" | "pre" | "em":
-                                self.description += text
-                            case "a":
-                                link = make_abs_link(e["href"])
-                                self.description += f"[{text}]({link})"
-                            case "br":
-                                self.description += "\n"
-                            case "img":
-                                link = make_abs_link(e["src"])
-                                if not link in self.files:
-                                    self.files.append(link)
-                                ext = os.path.splitext(link)[1]
-                                index = self.files.index(link)
-                                self.description += f"[img]({index:03}{ext})"
-                            case _:
-                                print("error: unknown element name")
-                                print(e)
-                                exit(1)
-                    self.description += "\n"
-        else:
-            self.description = None
+        if "path" in rev["file"]:
+            file = rev["file"]
+            url = build_full_url(file["path"])
+            if not self.has_file(url):
+                self.files.append([url, file["name"]])
+        for file in rev["attachments"]:
+            url = build_full_url(file["path"])
+            if not self.has_file(url):
+                self.files.append([url, file["name"]])
 
-        post_files = post_body.find("div", class_="post__files")
-        if post_files != None:
-            for thumbnail in post_files.find_all("div", class_="post__thumbnail"):
-                file = thumbnail.find("a", class_="fileThumb")
-                self.files.append(build_full_url(file["href"]))
-            for link in post_files.find_all("a", recursive=False):
-                self.files.append(link["href"])
+        bs = BeautifulSoup(rev["content"], "html.parser")
+        self.description = self.parse_content(bs)
 
-        self.attachments = []
-        post_attachments = post_body.find("ul", class_="post__attachments")
-        if post_attachments != None:
-            for attachment in post_attachments.find_all(
-                "a", class_="post__attachment-link"
-            ):
-                self.attachments.append(
-                    [build_full_url(attachment["href"]), attachment.text.strip()[9:]]
-                )
+    def parse_content(self, bs):
+        string = bs.string
+        if string != None:
+            return string
+
+        string = ""
+        pre = bs.find("pre")
+        if pre != None:
+            string += pre.text.strip()
+        ps = bs.find_all(["p", "h3"])
+        if len(ps) == 0:
+            ps = [bs]
+        for p in ps:
+            for e in p:
+                text = str(e.string).strip()
+                match e.name:
+                    case None | "strong" | "span" | "pre" | "em":
+                        string += text
+                    case "a":
+                        link = build_full_url(e["href"])
+                        string += f"[{text}]({link})"
+                    case "br":
+                        string += "\n"
+                    case "img":
+                        link = build_full_url(e["src"])
+                        if self.has_file(link):
+                            self.attachments.append([link, None])
+                        ext = os.path.splitext(link)[1]
+                        index = self.files.index(link)
+                        string += f"[img]({index:03}{ext})"
+                    case _:
+                        print("error: unknown element name")
+                        print(e)
+                        exit(1)
+            string += "\n"
+        return string
+
+    def has_file(self, url):
+        for file in self.files:
+            if file[0] == url:
+                return True
+        return False
 
 
 def list_post_urls(site, uid):
-    url = f"{origin}/{site}/user/{uid}/"
+    url = f"{origin}/api/v1/{site}/user/{uid}"
     result = []
-    while True:
-        res = fetch.request(url)
-        bs = BeautifulSoup(res.text, "html.parser")
-        section = bs.body.main.section
 
-        items = section.find("div", class_="card-list__items")
-        for post in items.find_all("article", class_="post-card"):
-            result.append(post.find("a")["href"].split("/")[-1])
-
-        paginator = section.find("div", id="paginator-top")
-        next_page = (
-            paginator.menu.find("a", class_="next") if paginator.menu != None else None
-        )
-        if next_page == None:
-            break
-        url = build_full_url(origin + next_page["href"])
-
-    return result
+    res = fetch.request(url)
+    arr = json.loads(res.text)
+    return [post["id"] for post in arr]
 
 
 def download(url, path):
@@ -149,8 +133,10 @@ def is_downloadable_link(link):
 
 
 def save_post(post, path):
-    if post.description == None and len(post.files) == 0 and len(post.attachments) == 0:
+    if post.description == None and len(post.files) == 0:
         return True
+
+    print("*", post.title, len(post.files))
 
     postdir = path + "/" + post_to_dirname(post)
     os.makedirs(postdir, exist_ok=True)
@@ -158,19 +144,33 @@ def save_post(post, path):
         text = post.description.strip()
         if len(text) != 0:
             open(postdir + "/info.txt", mode="w").write(text)
-
-    print("*", post.title, len(post.files))
+    if post.embed != None:
+        url = post.embed["url"]
+        subject = post.embed["subject"]
+        description = post.embed["description"]
+        text = f"[{description}]({url})"
+        open(postdir + f"/{subject}.txt", mode="w").write(text)
 
     failed = False
 
-    def run_workers(fn, n):
-        workers = []
-        for i in range(n):
-            thread = threading.Thread(target=fn)
-            thread.start()
-            workers.append(thread)
-        for w in workers:
-            w.join()
+    def download_file(post, i):
+        link, name = post.files[i]
+        print(i + 1, "/", len(post.files), link)
+        savepath = postdir + f"/{i:03}-{name}"
+        if not is_downloadable_link(link):
+            print("warn: unhandled link type, saving url")
+            savepath += ".txt"
+            open(savepath, "w").write(link)
+            return
+
+        ext = os.path.splitext(link)[1]
+        if ext == ".jpe" or ext == ".jpeg":
+            ext = ".jpg"
+        savepath += ext
+        if os.path.exists(savepath):
+            return
+        if not download(link, savepath):
+            failed = True
 
     job_index = atomic.AtomicInt()
 
@@ -179,46 +179,16 @@ def save_post(post, path):
 
         i = job_index.fetch_add()
         while i < len(post.files):
-            while True:
-                link = post.files[i]
-                print(i + 1, "/", len(post.files), link)
-                if is_downloadable_link(link):
-                    ext = os.path.splitext(link)[1]
-                    if ext == ".jpe" or ext == ".jpeg":
-                        ext = ".jpg"
-                    savepath = postdir + f"/{i:03}{ext}"
-                    if os.path.exists(savepath):
-                        break
-                    if not download(link, savepath):
-                        failed = True
-                else:
-                    print("warn: unhandled link type, saving url")
-                    ext = ".txt"
-                    open(postdir + f"/{i:03}{ext}", "w").write(link)
-                break
+            download_file(post, i)
             i = job_index.fetch_add()
 
-    run_workers(worker_main, 4)
-
-    job_index = atomic.AtomicInt()
-
-    def worker_main():
-        nonlocal failed
-
-        i = job_index.fetch_add()
-        while i < len(post.attachments):
-            while True:
-                attachment = post.attachments[i]
-                savepath = postdir + "/" + attachment[1]
-                print(attachment[0])
-                if os.path.exists(savepath):
-                    break
-                # print(attachment[0], attachment[1])
-                if not download(attachment[0], savepath):
-                    failed = True
-            i = job_index.fetch_add()
-
-    run_workers(worker_main, 4)
+    workers = []
+    for i in range(4):
+        thread = threading.Thread(target=worker_main)
+        thread.start()
+        workers.append(thread)
+    for w in workers:
+        w.join()
 
     return not failed
 
